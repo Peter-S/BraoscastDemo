72e3e64b07448d172725cb39ab02a92a94831391
Peter-S <32533746+Peter-S@users.noreply.github.com>
2018-06-08 19:22:46 -0700

    Initial commit

---
+++ app/src/main/java/com/example/lmnop/braoscastdemo/DbContract.java
@@ -0,0 +1,9 @@
+package com.example.lmnop.braoscastdemo;
+
+public class DbContract {
+
+    public static final String TABLE_NAME = "incomingInfo";
+    public static final String INCOMING_NUMBER = "incomingNumber";
+    public static final String UPDATE_UI_FILTER = "com.example.lmnop.braoscastdemo.UPDATE_UI";
+
+}
